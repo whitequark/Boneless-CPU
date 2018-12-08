c2d660f480563fbadba11d872dcd51f3f367341d
William D. Jones <thor0505@comcast.net>
2018-12-08 09:45:26 -0500

    Fill out setup.py.

--- setup.py
+++ setup.py
@@ -3,5 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name="boneless_sim",
     version="0.1.0",
-    packages=find_packages(exclude=["tests"])
+    description="Simulator for the Boneless CPU",
+    long_description=open("README.md").read(),
+    url="https://github.com/cr1901/boneless-sim",
+    author="William D. Jones",
+    author_email="thor0505@comcast.net",
+    license="BSD",
+    packages=find_packages(exclude=["tests"]),
+    platforms=["Any"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: System :: Emulators",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
 )
