27c94b0ff8ffa16625ba591f32cd9ddad2169132
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-12 19:00:04 +0800

    export descriptions from Tex docs

--- boneless/assembler/tools/grab.py
+++ boneless/assembler/tools/grab.py
@@ -2,7 +2,7 @@ import os
 
 base = "../../../doc/manual/insns/"
 li = os.listdir(base)
-print(li)
+data = {} 
 for i in li:
     f = open(base+i)
     d = f.readlines()
@@ -15,4 +15,11 @@ for i in li:
         if j.find("\\assembly") > 0:
             val = j.strip().split("}")
             reg = val[1].strip()
-    print(name,' ', reg , ";" ,descr)
+    #print(name,' ', reg , ";" ,descr)
+    data[name] = [name,reg,descr]
+
+s = 'descriptions ='+str(data)
+print(s)
+f = open('descriptions.py','w')
+f.write(s)
+f.close()
