c046b3ddb728e267ab94f7f1ab4edcfdb58d3ede
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-20 23:23:29 +0800

    cool , should be able to do ansi codes in a pty

--- boneless/simulator/terminal_test.py
+++ boneless/simulator/terminal_test.py
@@ -1,15 +1,27 @@
-import os, pty
+import os, pty,sys
 from serial import Serial
 import threading
 
 # lifted from
 # http://allican.be/blog/2017/01/15/python-dummy-serial-port.html
-
+b0 = "╔═╦═╗╓─╥─╖╒═╤═╕┌─┬─┐" .encode(sys.stdout.encoding)
+b1 = "║ ║ ║║ ║ ║│ │ ││ │ │" .encode(sys.stdout.encoding)
+b2 = "╠═╬═╣╟─╫─╢╞═╪═╡├─┼─┤" .encode(sys.stdout.encoding)
+b3 = "║ ║ ║║ ║ ║│ │ ││ │ │" .encode(sys.stdout.encoding)
+b4 = "╚═╩═╝╙─╨─╜╘═╧═╛└─┴─┘" .encode(sys.stdout.encoding)
+print(b0)
 def listener(port):
     #continuously listen to commands on the master device
+    #os.write(port,b"\x1b[500;500f")
+    #os.write(port,b"\x1b[6n")
+    #os.write(port,b"\x1b[41m")
+    #os.write(port,b"\x1b[39m")
+
+    #os.write(port,b"\x1b[2j")
+    ##os.write(port,b"\x1b[0;0")
     while 1:
         res = b""
-        while not res.endswith(b"\r\n"):
+        while not res.endswith(b"\r"):
             #keep reading one byte at a time until we have a full line
             v = os.read(port, 1)
             print(v)
@@ -17,11 +29,24 @@ def listener(port):
             os.write(port,v)
         print("command: %s" % res)
 
+        #os.write(port,b"\x1b[500;500f")
+        #os.write(port,b"\x1b[6n")
         #write back the response
         if res == b'QPGS\r\n':
             os.write(port, b"correct result\r\n")
         else:
-            os.write(port, b"I dont understand\r\n")
+            os.write(port,b"\x1b[48;5;171m")
+            #os.write(port,b"\x1b[39m")
+            os.write(port,b0)
+            os.write(port,b'\r\n')
+            os.write(port,b1)
+            os.write(port,b'\r\n')
+            os.write(port,b2)
+            os.write(port,b'\r\n')
+            os.write(port,b3)
+            os.write(port,b'\r\n')
+            os.write(port,b4)
+            os.write(port,b'\r\n')
 
 def test_serial():
     """Start the testing"""
