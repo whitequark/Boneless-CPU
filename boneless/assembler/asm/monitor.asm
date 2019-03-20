eb07c86350dfa772ecff7e8836a50181db2de53c
Simon Kirkby <obeygiantrobot@gmail.com>
2019-03-20 09:19:37 +0800

    start doc for the monitor , TinyFpgaBX ordered

---
+++ boneless/assembler/asm/monitor.asm
@@ -0,0 +1,13 @@
+; The serial monitor a simple monitor program
+; for teathering the CPU
+;
+; Needs to provide the following commands
+; READ : read a specific word from memory -> R , ADDR , VAL
+; WRITE : write a specific word to memory memory -> W , ADDR , VAL
+; CALL : jump the execution to a memory position -> J ADDR
+;
+; extras
+; bulk read
+; bulk write
+; running checksum
+; TODO write me
